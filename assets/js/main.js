bf4082d642dbec51e8e4f8c3732b863331be9c9c
João Leão <flocoswhite@gmail.com>
2023-12-07 21:12:22 -0300

    :sparkles: feat: adding the API

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,36 @@
 
-console.log("conexão feita");
+const offset = 0;
+const limit = 10;
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+
+function convertPokemonToHtml(pokemon) {
+    return `
+        <li class="pokemon">
+            <span class="number">#001</span>
+            <span class="name">${pokemon.name}</span>
+            <div class="detail">
+                <ol class="types">
+                    <li class="type">grass</li>
+                    <li class="type">poison</li>
+                </ol>
+                <img src="https://i.pinimg.com/originals/e2/44/3d/e2443d9017d005260988daaa839d768e.png"
+                    alt="${pokemon.name}">
+            </div>
+        </li>
+    `
+}
+
+const pokemonList = document.getElementById('pokemonList')
+
+fetch(url)
+    .then((response) => response.json())
+    .then((jsonBody) => jsonBody.results)
+    .then((pokemons) => {
+
+        for (let i = 0; i < pokemons.length; i++) {
+            const pokemon = pokemons[i];
+            pokemonList.innerHTML += convertPokemonToHtml(pokemon)
+        }
+
+    })
+    .catch((error) => console.error(error))
